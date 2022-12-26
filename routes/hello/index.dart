236925ffb27c06e97b44f20568be7705ac24f355
Alijahany59 <hamidjalili9010@gmail.com>
2022-12-26 23:53:56 +0330

    chore: create test route

--- routes/hello/index.dart
+++ routes/hello/index.dart
@@ -1,5 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'Route Not Found', statusCode: 404);
+  return Response(body: 'Welcome to G Z Server');
 }
